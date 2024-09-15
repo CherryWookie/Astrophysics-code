398420e69688236265ef4229644d133254dcd7b9
cherry_wookie <michaelthesell@gmail.com>
2024-09-15 14:20:47 +0200

    first commit

---
+++ hw1/hw1.py
@@ -0,0 +1,6 @@
+# This is my first assignment for Astrophysics
+# Also when getting git to work, I had to change the .json settings to usr/local/bin/git instead of usr/local/bin/git like everything else is
+# WTF why was it switched like that. Either way it's working now I suppose
+
+msg = "Finally World"
+print(msg)
