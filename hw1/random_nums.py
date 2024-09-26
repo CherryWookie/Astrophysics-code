3ad804191e61fd50f1aa06f53f8358e56df44fb5
cherry_wookie <michaelthesell@gmail.com>
2024-09-26 10:16:32 +0200

    randnum

---
+++ hw1/random_nums.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+
+# Random Number Generator
+# This one is bad though because it presents some correlation between numbers, although having a large period
+
+Z = 7 #Seed
+a = 2
+c = 4
+m = 2**31 #full period
+iters = 10
+output = []
+
+for n in range(iters):
+    Z_1 = (a * Z + c) % m
+    X = (65539 * Z) % m #RANDU?
+    Z = Z_1
+    output.append(X) # Change append arguement 
+
+print(output)
+
