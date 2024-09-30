983b190b14d41c47e3a5c33c131ea1197cff1147
cherry_wookie <michaelthesell@gmail.com>
2024-09-30 14:31:13 +0200

    commit message

--- hw1/random_nums.py
+++ hw1/random_nums.py
@@ -5,7 +5,7 @@ import numpy as np
 # This one is bad though because it presents some correlation between numbers, although having a large period
 
 Z_0 = 7 # Initial Seed
-a = 2
+a = 2 
 c = 4
 m = 23 # Full Period for RANDOM NUMBER
 n = 2**31 # Full Period for RANDU IBM
@@ -16,11 +16,17 @@ output_randu = []
 # Random Number with Z seed and N iterations
 
 for i in range(iters):
+    # Random number calculation
     Z_1 = (a * Z_0 + c) % m
+
+    # RANDU IBM number calculation
     randu = (65539 * Z_0) % n #RANDU?
+
     Z_0 = Z_1
+
+    # Append to respective lists
     output_rand.append(Z_1)
-    output_randu.append(randu) # Change append arguement 
+    output_randu.append(randu)
 
 print(f'RANDOM NUMBER: ', output_rand)
 print(f'RANDU IBM: ', output_randu)
