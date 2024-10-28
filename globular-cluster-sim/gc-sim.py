c3cfa8a2eba08068117a12e543e9ea34ab5aff73
cherry_wookie <michaelthesell@gmail.com>
2024-10-28 16:33:08 +0100

    done

--- globular-cluster-sim/gc-sim.py
+++ globular-cluster-sim/gc-sim.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 np.random.seed(9)
 
 # Create variables
-N = 1000 # Number of objects
+N = 10000 # Number of objects
 t_cluster = 10
 t_born = []
 t_ms = []
@@ -14,28 +14,35 @@ alpha = 2.35
 min_mass = 0.1
 max_mass = 100
 mass_ms = []
+mass_wd = []
 num_wd = 0
-num_bh = 0
-lum = []
+num_bh = 0 # Black Holes and Neutron Star count
+lum_ms = []
+lum_wd = []
 t_eff = []
-R = []
+t_eff_wd = []
+R_ms = []
+R_wd = []
 sigma = 5.67e-8 # W/m^2 K^4
+C = 0.0101
+R_solar = 6.969e8
+L_solar = 3.827e26
 
 # Monte Carlo Simulation
 while len(mass_ms) < N:
 
-    # Generate random number for comparison
-    rand_y = np.random.uniform(min_mass, max_mass**(-alpha))
+    # Generate random number for comparison between 1 and 0
+    rand_y = np.random.uniform(0,1)
 
     # Generate IMF with random mass vector between min-max mass
     rand_mass = np.random.uniform(min_mass, max_mass)
-    mass_i = rand_mass**(-alpha)
-    
+    mass_i = (rand_mass)**(-alpha) # Use mass_i as the probability function of acceptance 
 
-    # Accept Reject
+
+    # Accept Reject Method
     if rand_y < mass_i:
         # ACCEPT
-        mass_ms.append(mass_i)
+        mass_ms.append(rand_mass) # Accept the random mass in between min and max, not mass_i
     else: 
         # REJECT
         continue
@@ -55,67 +62,87 @@ for i in range(N):
     t_cool_i = t_cluster - t_born[i] - t_ms[i]
     t_cool.append(t_cool_i)
 
-# Finding luminosity and temperature, etc.
+# Finding luminosity and temperature, etc. for MS and WD
 for i in range(N):
 
     mass = mass_ms[i]
-    # print(mass)
 
-    # T_cool conditions to calculate Luminosity
-    if t_cool[i] < 0:
+    # Main Sequence Luminosity
+    if t_cool[i] <= 0:
         # Calculate Luminosity based on mass ranges
-        if mass >= 20:
-            lum_i = 81 * mass ** 2.14
-        elif 2 < mass <= 20:
-            lum_i = 1.78 * mass ** 3.5
-        elif mass <= 2:
-            lum_i = 0.75 * mass ** 4.8
-        lum.append(lum_i)
+        if mass > 55:
+            lum_i = 3200 * mass
+        elif 2 < mass <= 55:
+            lum_i = 1.4 * mass ** 3.5
+        elif 0.43 < mass <= 2:
+            lum_i = mass ** 4
+        elif mass <= 0.43:
+            lum_i = 0.23 * mass ** 2.3
+        lum_ms.append(lum_i)
 
-        # Calculate Radius R based on mass
+        # Main Sequence Radius
         if mass >= 1.2:
             R_i = 10 ** (0.66 * np.log(mass)) + 0.5
         else:
             R_i = mass
-        R.append(R_i)
+        R_ms.append(R_i)
 
-        # Calculate Effective Temperature T_eff
-        t_eff_i = (lum_i / (4 * np.pi * R_i**2 * sigma)) ** (1 / 4)
+        # Main Sequence Effective Temperature
+        t_eff_i = (lum_i * L_solar/ (4 * np.pi * R_i**2 * R_solar * sigma)) ** (1 / 4)
         t_eff.append(t_eff_i)
 
-        # Debug: Print values in the loop
         # print(f"mass[{i}] = {mass}, t_cool[{i}] = {t_cool[i]}, lum[{i}] = {lum_i}, R[{i}] = {R_i}, T_eff[{i}] = {t_eff_i}")
-    else:
-        print(f"mass[{i}] = {mass} did not meet t_cool condition, t_cool[{i}] = {t_cool[i]}")
+ 
+ 
+    # White Dwarf Conditions
+    elif t_cool_i > 0:
+        if mass_ms[i] < 10:
+            num_wd += 1 # White Dwarf
 
-# # Debug: Final output values
-# print("Final Luminosity:", lum)
-# print("Final Radius:", R)
-# print("Final T_eff:", t_eff)
+            # White Dwarf Mass
+            mass_wd_i = 0.49 * np.exp(0.095 * mass_ms[i])
+            mass_wd.append(mass_wd_i)
+            
+            # White Dwarf Radius
+            R_wd_i = C / mass_wd_i**(1/3)
+            R_wd.append(R_wd_i)
+
+            # White Dwarf Luminosity
+            lum_wd_i = 10**(-3) / t_cool[i]**(7/5)
+            lum_wd.append(lum_wd_i)
+
+            # White Dwarf T_eff
+            t_eff_wd_i = (lum_wd_i * L_solar / (4 * np.pi * R_wd_i**2 * R_solar * sigma)) ** (1 / 4)
+            t_eff_wd.append(t_eff_wd_i)
+
+        # Black Hole or Neutron Star conditions
+        elif mass_ms[i] > 10:
+            num_bh += 1 # Black Hole or Neutron Star
+            # print(f"mass[{i}] = {mass} did not meet t_cool condition, t_cool[{i}] = {t_cool[i]}")
 
 
+#___________________________________________________________________
+# PLOTS:
+#___________________________________________________________________
 
-    # elif t_cool_i > 0:
-    #     if mass_ms[i] < 10:
-    #         num_wd += 1
+# Create Gaussian Noise Main Sequence
+lum_noisy = lum_ms * (1 + np.random.normal(0, 0.1, size=len(lum_ms)))
+temp_noisy = t_eff * (1 + np.random.normal(0, 0.1, size=len(t_eff)))
 
-    #     elif mass_ms[i] > 10:
-    #         num_bh += 1
+# Create Gaussian Noise White Dwarf
+lum_wd_noisy = lum_wd * (1 + np.random.normal(0, 0.1, size=len(lum_wd)))
+temp_wd_noisy = t_eff_wd * (1 + np.random.normal(0, 0.1, size=len(t_eff_wd)))
 
-    #     Yes -- M_ms < 10 -- yes --> White Dwarf --> Lum, T_eff WD. if > 10, no, BH (Black hole) NS (Neutron star)
 
-# print(f"Luminosity: {lum}")
-# print(f"Radius: {R}")
-# print(f"MASSES: {mass_ms}")
-
+# Debug, Masses from largest to smallest
 sorted_arr = sorted(mass_ms, reverse=True)
+print("Sorted array from largest to smallest:", sorted_arr)
 
-# print("Sorted array from largest to smallest:", sorted_arr)
-# print(f"Number of Black Holes or Neutron Stars: {num_bh}")
-# print(f"Number of White Dwarfs: {num_wd}")
-# # print(len(t_ms))
+# Print Number of Black Holes and Neutron Stars, as well as White Dwarfs
+print(f"Number of Black Holes or Neutron Stars: {num_bh}")
+print(f"Number of White Dwarfs: {num_wd}")
 
-# Plot Mass 
+# Plot Original Mass 
 plt.hist(mass_ms, bins=100, log=True, edgecolor='black')
 plt.xlabel('Mass (Msun)')
 plt.ylabel('Number of Stars')
@@ -129,14 +156,46 @@ plt.ylabel('Number of Stars')
 plt.title('TBORN')
 plt.show()
 
-# Plot Luminosity vs T_eff
-plt.scatter(t_eff, lum)
+# Plot Main Sequence Luminosity vs T_eff
+plt.scatter(t_eff, lum_ms)
+
+# Plot White Dwarf Luminosity vs T_eff_wd
+plt.scatter(t_eff_wd, lum_wd)
 plt.yscale('log')
-# plt.xscale('log')
 plt.xlabel('T_eff')
 plt.ylabel('Luminosity')
 plt.title('Plot of Temperature vs Luminosity')
-plt.gca().invert_yaxis()
+plt.gca().invert_xaxis()
 plt.show()
 
+# Plot White Dwarf / Main Sequence Luminosity vs T_eff_wd LOG-LOG
+plt.scatter(t_eff, lum_ms)
+plt.scatter(t_eff_wd, lum_wd)
+plt.yscale('log')
+plt.xscale('log')
+plt.xlabel('T_eff')
+plt.ylabel('Luminosity')
+plt.title('Plot of Temperature vs Luminosity')
+plt.gca().invert_xaxis()
+plt.show()
 
+# Plot with 10% Gaussian Noise
+plt.scatter(temp_noisy, lum_noisy, s=5)
+plt.scatter(temp_wd_noisy, lum_wd_noisy, s=5)
+plt.yscale('log')
+plt.xlabel('T_eff')
+plt.ylabel('Luminosity')
+plt.title('Plot of Temperature vs Luminosity')
+plt.gca().invert_xaxis()
+plt.show()
+
+# Plot with 10% Gaussian Noise LOG-LOG
+plt.scatter(temp_noisy, lum_noisy, s= 5)
+plt.scatter(temp_wd_noisy, lum_wd_noisy, s = 5)
+plt.yscale('log')
+plt.xscale('log')
+plt.xlabel('T_eff')
+plt.ylabel('Luminosity')
+plt.title('Plot of Temperature vs Luminosity')
+plt.gca().invert_xaxis()
+plt.show()
